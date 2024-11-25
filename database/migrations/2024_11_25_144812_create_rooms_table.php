197934061419f2c6f15872f288c368a5aad5dfbb
Bobur Farhodov <farhodovbobur@MacBook-Pro-Bobur.local>
2024-11-25 21:00:19 +0500

    MINOR: create room_user pivot table

--- database/migrations/2024_11_25_144812_create_rooms_table.php
+++ database/migrations/2024_11_25_144812_create_rooms_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('channels', function (Blueprint $table) {
+        Schema::create('rooms', function (Blueprint $table) {
             $table->id();
             $table->enum('type', ['group', 'contact', 'channel'])->nullable();
             $table->string('channel')->unique()->nullable();
